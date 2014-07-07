a2599e1418c630951a429cccbf2a66d31bdf38f6
Petr Kramar <kramar@masquerade.cz>
2014-07-07 23:57:45 +0200

    Creating ohlc frames without using for loop TODO frame size is now hardcoded

--- ticksToOhcl.R
+++ ticksToOhcl.R
@@ -1,30 +1,32 @@
-ticksToOhcl<-function(tickData,timeFrameSize=30){
-  #converts tick data to ohcl format(open,high,close,low)
+ticksToOhcl<-function(tickData,timeFrameSize=60, timeFormat='%Y%m%d %H:%M:%OS'){
+  
+  # convert factors to date object
+  tickData$Date = strptime(tickData$Date, timeFormat)
+  
+  # round date to timeFrameSize
+  # all date from the same ohlc frame has to have 
+  # same date for later aggregation
   #
-  #tickData has to have columns: 
-  #"date" - format "yyyymmdd hh:mm:ss.xxx (where xxx are miliseconds)
-  #"buy" and "sell"
-  #
+  # now hardcoded 30s size of frame
+  # tickData$Date$sec = floor(tickData$Date$sec/30)*30
+  tickData$Date$sec = floor(tickData$Date$sec/60)
   
-#   > mydate = strptime(b,"%Y%m%d %H:%M:%S.%OS")
-#   > mydate$sec
-#   [1] 2
-#   > mydate
-#   [1] "2014-05-01 00:00:02 CEST"
-#   > mydate$sec
-#   [1] 2
-#   > mydate$min
-#   [1] 0
-#   > mydate$hour
-#   [1] 0
-#   > 
+  # open
+  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="head",n=1)
+  Open = tmp[,2]
   
-  lastTick = NULL
+  # high
+  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="max")
+  High = tmp[,2]
   
-  for(i in 1:nrow(tickData)){
-    tick = tickData[i,]
-    
-    
-  }
+  # low
+  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="min")
+  Low = tmp[,2]
   
+  # Close
+  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="tail",n=1)
+  Close = tmp[,2]
+  Date = tmp[,1]
+  
+  data.frame(Date,Open,High,Low,Close)
 }
