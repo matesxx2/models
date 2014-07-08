5ca1de347be3f66a89d686b6314dc2c50c273ead
matesxx2 <matesxx2@users.noreply.github.com>
2014-07-08 14:40:04 +0200

    Update ticksToOhcl.R

--- ticksToOhcl.R
+++ ticksToOhcl.R
@@ -12,22 +12,17 @@ ticksToOhcl<-function(tickData,timeFrameSize=60, timeFormat='%Y%m%d %H:%M:%OS'){
   tickData$Date$sec = rep(0,nrow(tickData))
   tickData$Date$min = floor(tickData$Date$min/2)*2
   
-  # open
-  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="head",n=1)
-  Open = tmp[,2]
+  # http://stackoverflow.com/questions/12064202/using-aggregate-for-multiple-aggregations
+  #
+  # Since there is a bug? (2013-08-08) in aggregate 
+  # you can not save the output of aggregate(data.frame) in a object of data.frame class. 
+  #To get a proper result you have to:
+  # object <- as.data.frame(as.list(aggregate(data.frame)))
+  #
+  res = as.data.frame(as.list(aggregate(tickData$Buy,
+                  by=list(as.character(tickData$Date)),
+                  FUN=function(x) c(Open=head(x,1), High=max(x), Low=min(x), Close=tail(x,1)))))
   
-  # high
-  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="max")
-  High = tmp[,2]
-  
-  # low
-  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="min")
-  Low = tmp[,2]
-  
-  # Close
-  tmp = aggregate(x=tickData$Buy,by=list(as.character(tickData$Date)),FUN="tail",n=1)
-  Close = tmp[,2]
-  Date = tmp[,1]
-  
-  data.frame(Date,Open,High,Low,Close)
+  colnames(res)<-c("Date", "Open", "High", "Low", "Close")
+  res
 }
